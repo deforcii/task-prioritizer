c79793db146d500453ad04a4066c2a18328b2771
deforcii <2006monder@gmail.com>
2025-05-22 00:00:03 -0700

    Initial commit

---
+++ Task.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Task {
+        private String title;
+        private String description;
+        private LocalDate deadline;
+        private int importance;
+    
+        // Constructor
+        // Getters and Setters
+        // toString() for printing
+}
